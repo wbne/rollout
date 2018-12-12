a3558c7658fd32c8ff33f4b8703b5c7538c8250f
wbne <firetruckbw@gmail.com>
2018-12-11 21:32:25 -0600

    Add files via upload

--- bleb.cpp
+++ bleb.cpp
@@ -8,22 +8,38 @@ int main()
     //caps frame rate at monitor refresh rate
     window.setVerticalSyncEnabled(true);
 
+    int point = 0;
+    int rando = 0;
     //Player 1
     sf::CircleShape blebby(30.f);
     blebby.setFillColor(sf::Color::White);
     bool inAir = true;
     bool hasSpeed = false;
+    int blebscore = 0;
     //Player 1 hit box
     sf::FloatRect boundingBox;
     //Player 1 velocity and spawn
     sf::Vector2f blebv(0, 0);
     blebby.setPosition(50.f, 50.f);
+    double blebspeed = 0;
+
+    //Player 2
+    sf::CircleShape blebster(30.f);
+    blebster.setFillColor(sf::Color::Black);
+    bool inAirster = true;
+    bool hasSpeedster = false;
+    int blebsterscore = 0;
+    //Player 2 hit box
+    sf::FloatRect boundingBoxster;
+    //Player 2 velocity and spawn
+    sf::Vector2f blebsterv(0, 0);
+    blebster.setPosition(150.f, 150.f);
+    double blebsterspeed = 0;
+
     //Floor
     sf::RectangleShape floor(sf::Vector2f(800.f, 160.f));
     floor.setFillColor(sf::Color::Green);
     floor.setPosition(sf::Vector2f(0.f, 500.f));
-
-    //TODO: add player 2 and actual game play
 
     //loop boi
     while (window.isOpen())
@@ -43,10 +59,25 @@ int main()
                         }
                     if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) //left
                         blebv.x += -1;
-                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) //right
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) //down
                         blebv.y += 1;
-                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) //down
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) //left
                         blebv.x += 1;
+                        //p2 stuff
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) //up
+                    if(!inAirster)
+                        {
+                            blebsterv.y += -20;
+                            inAirster = true; //only jump on ground
+                        }
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) //left
+                        blebsterv.x += -1;
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) //down
+                        blebsterv.y += 1;
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) //right
+                        blebsterv.x += 1;
+                    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+                        std::cout<<blebscore<<"-"<<blebsterscore<<std::endl;
                     break;
             default: break; //for later?
             }
@@ -56,17 +87,68 @@ int main()
 
         if(inAir)
             blebv.y += 1; //gravity
-        if(std::pow(blebv.x, 2) + std::pow(blebv.y,2) > 500)
+        if(inAirster)
+            blebsterv.y +=1; //grav 2
+        if(blebspeed > 400)
         {
             hasSpeed = true; //checks if player 1 is fast enough for magic
-            std::cout<<"SPEED:"<<blebv.x<<" "<<blebv.y<<std::endl;
+           //std::cout<<"SPEED:"<<blebv.x<<" "<<blebv.y<<std::endl;
         }
         else
             hasSpeed = false;
+        if(blebsterspeed > 400) //p2 speed
+        {
+            hasSpeedster = true; //checks if player 1 is fast enough for magic
+            //std::cout<<"SPEEDster:"<<blebsterv.x<<" "<<blebsterv.y<<std::endl;
+        }
+        else
+            hasSpeedster = false;
+
+            //calculates blebs speeds
+        blebspeed = std::pow(blebv.x, 2) + std::pow(blebv.y, 2);
+        blebsterspeed = std::pow(blebsterv.x, 2) + std::pow(blebsterv.y, 2);
+
+        //actual game thing
+        if(boundingBox.intersects(boundingBoxster))
+        {
+            //std::cout<<hasSpeed<<hasSpeedster<<std::endl;
+            point = 0;
+            rando = rand() % 800;
+            if(hasSpeed && hasSpeedster)
+                if(blebsterspeed > blebspeed)
+                    point = 2;
+                else
+                    point = 1;
+            else if(hasSpeed)
+                point = 1;
+            else if(hasSpeedster)
+                point = 2;
+            //std::cout<<"blebby: "<<blebscore<<std::endl;
+            //std::cout<<"blebster: "<<blebsterscore<<std::endl;
+            switch(point)
+            {
+                case 1:
+                    blebscore++;
+                    blebster.setPosition(rando,0);
+                    inAirster = true;
+                    break;
+                case 2:
+                    blebsterscore++;
+                    blebby.setPosition(rando,0);
+                    inAir = true;
+                    break;
+                default:
+                    break;
+            }
+            hasSpeed = false;
+            hasSpeedster = false;
+        }
 
         blebby.move(blebv); //displacement applied from vector
+        blebster.move(blebsterv); //displacement from vector 2
         window.clear(sf::Color::Cyan); //sky
         window.draw(blebby); //drawing player 1 after moving
+        window.draw(blebster); //drawing p2
         window.draw(floor); //drawing floor
         window.display(); //render
 
@@ -86,6 +168,23 @@ int main()
             blebv.y = 0;//makes you grounded and sets your position
             blebby.setPosition(blebby.getPosition().x, 440.f);
             inAir = false;
+        }
+        boundingBoxster = blebster.getGlobalBounds(); //checks for player 2 coords
+        if(blebster.getPosition().x < 0) //left side
+        {
+           blebsterv.x = 0; //forcibly moves player within the screen
+           blebster.setPosition(0, blebster.getPosition().y);
+        }
+        else if(blebster.getPosition().x > 740) //right side
+        {
+            blebsterv.x = 0;//same here
+            blebster.setPosition(740, blebster.getPosition().y);
+        }
+        if(blebster.getPosition().y > 440) //floor "collision"
+        {
+            blebsterv.y = 0;//makes you grounded and sets your position
+            blebster.setPosition(blebster.getPosition().x, 440.f);
+            inAirster = false;
         }
             //no sky hit box because I feel it's not needed
 
